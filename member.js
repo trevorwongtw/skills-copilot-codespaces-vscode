ea24bf4f7ddd72aa39673648d3a319b32886758e
Trevor Wong <44539644+trevorwongtw@users.noreply.github.com>
2024-12-10 07:22:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    const member = {
+        name: 'John',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function() {
+            console.log('Hello');
+        }
+    }
+
+    return member;
+}
